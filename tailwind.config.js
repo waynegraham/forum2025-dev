c5f304a4856d37e245665fe1d8c1de4b5b91c3a2
Wayne Graham <wgraham@clir.org>
2025-02-10 13:27:33 -0500

    Update accessibility issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ const withMT = require("@material-tailwind/html/utils/withMT");
 module.exports = withMT({
   content: [
     "./src/**/*.{html,njk}",
-    "../node_modules/tw-elements/js/**/*.js"
+    //"./node_modules/tw-elements/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [ require("tw-elements/plugin.cjs")],
+  //plugins: [ require("tw-elements/plugin.cjs") ],
+  darkMode: "class"
 });

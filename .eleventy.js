20cc649212205ed5f77f223ca42cb7547aceb0f8
Wayne Graham <wgraham@clir.org>
2025-03-18 09:41:55 -0400

    Updates and add Call for Proposals

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,19 @@
 const fs = require("fs");
 const htmlmin = require("html-minifier-terser");
 const { type } = require("os");
+const { DateTime } = require("luxon");
 
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime"); // https://github.com/5t3ph/eleventy-plugin-emoji-readtime
 
 const { format } = require("path");
 
 // 11ty plugins
 const eleventyPluginHubspot = require('eleventy-plugin-hubspot');
 
-module.exports = function(eleventyConfig) {
+module.exports = async function(eleventyConfig) {
+  const { IdAttributePlugin } = await import("@11ty/eleventy");
 
   if (process.env.ELEVENTY_PRODUCTION) {
     eleventyConfig.addTransform("htmlmin", htmlminTransform);
@@ -35,6 +38,11 @@ module.exports = function(eleventyConfig) {
     pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
   }
 
+  // Shortcodes
+  eleventyConfig.addPlugin(emojiReadTime, {
+		emoji: "📖",
+	});
+
   // Filters
   eleventyConfig.addFilter("usd", function(value) {
     if (typeof value !== 'number') {
@@ -45,6 +53,30 @@ module.exports = function(eleventyConfig) {
       currency: 'USD',
     }).format(value);
   });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+		if (!Array.isArray(array) || array.length === 0) {
+			return [];
+		}
+		if (n < 0) {
+			return array.slice(n);
+		}
+
+		return array.slice(0, n);
+	});
+
+  eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
+			format || "dd LLL yyyy"
+		);
+	});
+
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+	});
 
   // Plugins
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
@@ -68,6 +100,28 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyPluginHubspot, {
     portalId: 20251227
   });
+
+  // https://www.11ty.dev/docs/plugins/id-attribute/
+  eleventyConfig.addPlugin(IdAttributePlugin, {
+		selector: "h1,h2,h3,h4,h5,h6", // default
+
+		// swaps html entities (like &amp;) to their counterparts before slugify-ing
+		decodeEntities: true,
+
+		// check for duplicate `id` attributes in application code?
+		checkDuplicates: "error", // `false` to disable
+
+		// by default we use Eleventy’s built-in `slugify` filter:
+		slugify: eleventyConfig.getFilter("slugify"),
+
+		filter: function({ page }) {
+			if(page.inputPath.endsWith("test-skipped.html")) {
+				return false; // skip
+			}
+
+			return true;
+		}
+	});
 
   return {
     dir: {
